4a7cdf04510b6555d4a86526e070043422ced172
Maxime <payagemaxime@gmail.com>
2019-05-06 14:48:16 +0200

    Modification de index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-phpInfo();
+//$version = "7";
+//$message = "Je fais du PHP $version";
+//echo $message;
+
+$version = 7;
+$message = <h1>Je fais du PHP ' . $version . '</h1>;
+echo $message;
+
+define('TVATxPlein', 0.2);
